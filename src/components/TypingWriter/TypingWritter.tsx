6c35f1709dec4bc836f4944327c3dcc6717c020b
Joffre Quintas <112725262+joffrequintas@users.noreply.github.com>
2023-03-24 20:13:51 -0300

    Typing pronto

--- src/components/TypingWriter/TypingWritter.tsx
+++ src/components/TypingWriter/TypingWritter.tsx
@@ -5,31 +5,55 @@ import './TypingWritter.scss'
 
 export function TypingWritter() {
     const [text, setText] = useState('')
+    const [text2,setText2] = useState('')
 
+    const messages = [' Web Developer', ' Estudante de SI', ' Pai de Pet']
+
+    const messageDefault = 'Me chamo Joffre Quintas e sou ';
+        
+    function typeAndErase(msg, pos) {
+        let index = 0;   
+        const type = setInterval(() => {
+            setText2(currentText => currentText + msg[pos][index])
+            index++
+            if (index >= msg[pos].length -1) {
+                clearInterval(type)
+                setTimeout(() => {
+                    const erase = setInterval(()=> {
+                        setText2((currentText) => currentText.substring(0, currentText.length -1))
+                        index--
+                        if(index == 0) {
+                            clearInterval(erase)
+                        }
+                    },75)
+                },2000)
+            }
+        },75)
+    } 
+    
     useEffect(() => {    
-            const newText = 'Me chamo Joffre Quintas e sou Web Desenvolvedor'
-            const newTextArray = newText.split('')
-
+            
             let index = -1;
             const typing = setInterval(() => {
                 index++
-                setText(prevtext => prevtext + newTextArray[index])
-
-                if(index >= newTextArray.length - 1) {
+                setText(currentText => currentText + messageDefault[index])
+                if (index >= messageDefault.length-1) {
                     clearInterval(typing)
-
-                    const erasing = setInterval(() => {
-                        setText(prevText => prevText.substring(0, prevText.length-1)) 
-                        if (index <= 23) {
-                            clearInterval(erasing)
-                        }
-                        index--                        
+                    
+                    typeAndErase(messages,0)
+                    let pos = 1;
+                    setInterval(() => {
+                        typeAndErase(messages,pos)
+                        pos++
                         
-                    },75)
+                        if(pos >= messages.length) {
+                            pos = 0;   
+                        }
+                    },5000)
                 }
-            },75)
-            
+            },75)          
     },[])
     
-    return <h3>{text}<span>|</span></h3>
+    return <h3>{text}<b className="alternativeText">{text2}</b><span>|</span></h3>
 }
+// messages[pos].length*2*75 
